13b588a0577bfb0472347a51216cc7676f41e767
Blehjo <blehjooo@gmail.com>
2022-02-03 21:50:08 -0600

    creating startGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,3 +88,75 @@ function setTime() {
         
     }, 1000); 
 }
+
+
+function next() {
+    if (questions[0]) {
+        displayQuestion()
+    } else {
+        displayScore()
+    }
+}
+
+// function wrongOrRight() {
+//     if ()
+// }
+
+function displayQuestion() {
+    questionSection.innerHTML = "";
+    containerEL.innerHTML = "";
+    var questionsEL = document.createElement('div');
+    questionsEL.className = 'flex-column flex-center template';
+    var question = document.createElement('h2');
+    question.textContent = questions[0].question;
+    question.className = 'question'
+    containerEL.appendChild(questionsEL);
+    questionsEL.appendChild(question);
+    for (i = 0; i < 4; i++) {
+        var response = document.createElement('button');
+        response.className = 'questionbtn'
+        response.textContent = questions[0].options[i];
+        response.id = questions[0].options[i];
+        response.addEventListener("click", next);
+        questionsEL.appendChild(response);
+    }
+    
+    console.log(score)
+    // Implement 
+    questions.shift()
+}
+
+function squizGame() {
+    setTime();
+    //  Display question function
+    displayQuestion();
+}
+
+
+
+function startGame() {
+    questionSection.innerHTML = '';
+    var questionsEL = document.createElement('div');
+    questionsEL.className = 'flex-column flex-center';
+    questionsEL.id = 'starter';
+    var header = document.createElement('h2');
+    header.textContent = 'Some Rules of this Quiz';
+    var body = document.createElement('h3');
+    body.className = 'bodytext';
+    body.innerHTML = "1. You will have only 15 seconds per each question. \n 2. Once you select your answer, it can't be undone \n 3. You can't select any option once time goes off \n 4. You can't exit from the Quiz while you're playing \n 5. You'll get points on the basis of your correct answers";
+    containerEL.appendChild(questionsEL);
+    questionsEL.appendChild(header);
+    questionsEL.appendChild(body);
+    var start = document.createElement('button');
+    var quit = document.createElement('button');
+    start.textContent = 'Start';
+    quit.innerHTML = 'Quit';
+    start.className = 'btn';
+    quit.className = 'btn';
+    questionsEL.appendChild(start);
+    questionsEL.appendChild(quit);
+    start.addEventListener('click', squizGame);
+    quit.addEventListener('click', function() {
+        window.location.reload();
+    })
+};
